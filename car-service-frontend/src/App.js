7946c6a8e755b94dd2a86c558ccddd354618a51e
Ahmed-hagag8 <a7med7agag13579@gmail.com>
2026-02-27 23:03:28 +0200

    install dark mode

--- car-service-frontend/src/App.js
+++ car-service-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import { Car } from 'lucide-react';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { LanguageProvider } from './context/LanguageContext';
